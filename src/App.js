8caa8c84ab366c4f77cb1346c5dbb4393af0a695
Ismael Jarmouni <ijarmounigomez@gmail.com>
2022-03-25 20:08:57 +0100

    First commit with all changes - Ismael

--- src/App.js
+++ src/App.js
@@ -1,70 +1,252 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { createUseStyles } from "react-jss";
 import { useWebcamCapture } from "./useWebcamCapture";
-// import logo from './logo.svg'
-import logo from "./slap.png";
+//imported Stickers
+import slap from "./stickers/slap.png";
+import doing_fine from "./stickers/doing_fine.png";
+import frame from "./stickers/frame.png";
+import suspend from "./stickers/suspend.png";
+import poke from "./stickers/poke.png";
+import applause from "./stickers/applause.png";
+import grab from "./stickers/grab.png";
 
 import { Link, Switch, Route, Redirect } from "react-router-dom";
 
 const useStyles = createUseStyles((theme) => ({
-  "@global body": {
-    background: theme.palette.background,
-    color: theme.palette.text,
-    fontFamily: "sans-serif",
+  "@global *": {
+      margin: 0,
+      padding: 0,
+      fontFamily: 'Poppins',
+      scrollBehavior:"smooth",
+  },
+  
+  App: {
+    "& a": {
+      color: theme.palette.black_txt,
+      textDecorationLine: "none",
+    },
+    "& ul li": {
+      listStyleType: "none",
+    }
   },
 
-  App: {
-    padding: "20px",
-    background: theme.palette.primary,
-    maxWidth: "800px",
-    minHeight: "600px",
-    margin: "auto",
-    "& a": {
-      color: theme.palette.text,
+  Header: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    marginLeft: "2rem",
+    paddingLeft: "2rem",
+    backgroundColor: theme.palette.grey_background,
+    filter: theme.filters.light_grey_shadow,
+    "& h1": {
+      color: theme.palette.red_primary,
+      margin: 0,
+      fontSize: "6rem",
+    },
+    "& p": {
+      color: theme.palette.red_secondary,
+      fontSize: "2rem",
+      fontWeight: 600,
+      marginTop: ".5rem",
+      maxWidth: "30%"
+    },
+    "& span": {
+      textDecoration: "underline 3px #FE5F55",
+    },
+    "& nav": {
+      marginTop: "1.5rem",
+      fontSize: "1.5rem",
+      lineHeight: "2rem",
+    },
+    "& nav ul li a": {
+      color: theme.palette.red_nav,
+      fontWeight: 500,
+    },
+    "& nav ul li a:hover": {
+      opacity: 0.6,
+      transition: "0.2s ease-in-out",
     },
   },
-  Header: {
-    "&  h1": {
-      fontFamily: "sans-serif",
-      cursor: "pointer",
+
+  Gallery: {
+    display: "flex",
+    flexDirection: "column",
+    padding: "2rem 0 2rem 2rem",
+    
+    alignItems: "flex-start",
+    filter: theme.filters.light_grey_shadow,
+    minHeight:"50vh",
+    background: "#FCCDB5",
+    "& p": {
       fontSize: "4rem",
     },
+    "& span": {
+      textDecoration: "underline 3px #FE5F55",
+      fontWeight:500,
+    },
+    "& input:hover": {
+      maxWidth: "23rem",
+      minHeight: "4rem",
+      padding: ".5rem",
+      border: "5px solid black",
+      borderRadius: "10px",
+      textAlign: "center",
+      fontSize: "2.5em",
+      filter: "drop-shadow(9px 9px 0 #FE5F55)",
+      transition: "110ms",
+      outline: "none",
+    },
+    "& input": {
+      margin: "3rem 7rem 0",
+      maxWidth: "23rem",
+      minHeight: "4rem",
+      padding: ".5rem",
+      border: "5px solid black",
+      borderRadius: "10px",
+      textAlign: "center",
+      fontSize: "2.5em",
+      fontWeight: 600,
+      filter: "drop-shadow(5px 5px 0 #FE5F55)",
+      transition: "110ms",
+      outline: "none",
+    },
+    "& img": {
+      maxWidth: "40rem",
+    },
   },
-  Main: {
-    background: theme.palette.secondary,
 
+  Stickers: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "50vh",
+    marginLeft: "2rem",
+    alignItems: "flex-start",
+    padding: "2rem 0 2rem 2rem",
+    backgroundColor: theme.palette.stickers_background,
+    filter: theme.filters.light_grey_shadow,
+    "& p": {
+      fontSize: "4rem",
+    },
+    "& span": {
+      textDecoration: "underline 3px #FE5F55",
+      fontWeight:500,
+    },
+    "& button": {
+      border:"none",
+      padding: "1rem",
+      cursor: "pointer",
+      borderRadius: "10px",
+      background:"none",
+    },
+    "& img": {
+      maxWidth: "15em",
+    },
+    "& div": {
+      marginTop: "2rem",
+      display: "grid",
+      gap: "1.5rem",
+      gridTemplateColumns: "1fr 1fr 1fr",
+      maxWidth:"100%",
+      "@media (max-width: 1024px)": {
+        gridTemplateColumns:"1fr 1fr"
+      },
+      "@media (max-width: 670px)": {
+        gridTemplateColumns:"1fr"
+      }
+    },
+  },
+
+  Main: {
+    display: "flex",
+    padding: "2rem 0 2rem 2rem",
+    flexDirection: "column",
+    justifyContent:"center",
+    minHeight:"50vh",
+    backgroundColor: theme.palette.gallery_background,
+    filter: theme.filters.light_grey_shadow,
     "& canvas": {
-      width: "100%",
-      height: "auto",
+      maxWidth: "50rem",
+      padding: "4rem 0 5rem 5rem",
+
+      filter: theme.filters.canvas_shadow,
     },
     "& video": {
       display: "none",
     },
-  },
-  Stickers: {
-    "& img": {
-      height: "4rem",
+    "& p": {
+      fontSize: "4rem",
+    },
+    "& span": {
+      textDecoration: "underline 3px #FE5F55",
+      fontWeight:500,
     },
   },
-  Gallery: {
-    "& img": {
-      height: "16rem",
+
+  Footer: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "60vh",
+    marginLeft: "2rem",
+    alignItems: "flex-start",
+    padding: "2rem 0 0 2rem",
+    backgroundColor: theme.palette.red_primary,
+    color: theme.palette.text,
+    filter: theme.filters.light_grey_shadow,
+    "& p": {
+      fontSize: "4rem",
+    },
+    "& span": {
+      fontWeight:600,
+      textDecoration: "underline 5px #D64045",
     },
   },
+
   Picture: {
-    background: "black",
-    padding: 4,
-    position: "relative",
-    display: "inline-block",
-    "& h3": {
-      padding: 8,
-      textAlign: "center",
-      width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& img": {
+      maxWidth: "40rem",
+      filter: theme.filters.canvas_shadow,
     },
+    "& h3": {
+      padding: ".2rem .5rem 0 .5rem",
+      textAlign: "center",
+      maxWidth: "50rem",
+      fontSize: "2rem",
+    },
+    "& p": {
+      marginBottom: "2rem",
+    }
+  },
+
+  Readme: {
+    padding: "0 2rem 2rem",
+    fontWeight: 300,
+    maxWidth: "50%",
+    minHeight: "95vh",
+    fontSize: "1.3rem",
+    "& h2": {
+      fontSize: "5rem",
+      color: theme.palette.red_primary,
+    },
+    "& h3": {
+      textDecoration: "underline 5px #FE5F55",
+      fontSize: "2rem",
+      fontWeight: 700,
+      margin: "1rem 0 1rem"
+    },
+    "& section": {
+      padding: "2rem",
+      backgroundColor: theme.palette.grey_background,
+      filter: theme.filters.grey_shadow,
+    }
   },
 }));
 
-const stickers = [logo].map((url) => {
+
+const stickers = [slap,doing_fine,frame,poke,suspend,applause, grab].map((url) => {
   const img = document.createElement("img");
   img.src = url;
   return { img, url };
@@ -76,7 +258,7 @@ function App(props) {
   // currently active sticker
   const [sticker, setSticker] = useState();
   // title for the picture that will be captured
-  const [title, setTitle] = useState("SLAPPE!");
+  const [title, setTitle] = useState("Hello!");
 
   // webcam behavior hook
   const [
@@ -89,10 +271,13 @@ function App(props) {
   return (
     <div className={classes.App}>
       <header className={classes.Header}>
-        <h1>SlapSticker</h1>
+        <h1>StickerCam.</h1>
         <p>
-          Have you ever said something so dumb, you just wanted to slap
-          yourself? Well now you can!
+          Have you ever wanted to poke your face, slap it or even have someone smiling telling you you're doing fine? Well now you can!
+        </p>
+        <br />
+        <p>
+          <span>Scroll down</span> and follow the steps :P
         </p>
         <nav>
           <ul>
@@ -110,21 +295,38 @@ function App(props) {
         <Route path="/" exact>
           <main>
             <section className={classes.Gallery}>
-              Step one: Give it a name
+              <p>One: <span>Give it a name</span></p>
               <input
                 type="text"
-                value={title}
+                placeholder='Hello!'
                 onChange={(ev) => setTitle(ev.target.value)}
               />
             </section>
             <section className={classes.Stickers}>
-              Step 2: select your sticker...
+              <p>Two: <span>Select your sticker!</span></p>
+              <div>
               <button onClick={() => setSticker(stickers[0])}>
                 <img src={stickers[0].url} />
               </button>
+              <button onClick={() => setSticker(stickers[1])}>
+                <img src={stickers[1].url} />
+              </button>
+              <button onClick={() => setSticker(stickers[2])}>
+                <img src={stickers[2].url} />
+              </button>
+              <button onClick={() => setSticker(stickers[3])}>
+                <img src={stickers[3].url} />
+              </button>
+              <button onClick={() => setSticker(stickers[4])}>
+                <img src={stickers[4].url} />
+              </button>
+              <button onClick={() => setSticker(stickers[5])}>
+                <img src={stickers[5].url} />
+              </button>
+              </div>
             </section>
             <section className={classes.Main}>
-              Step three: Slap your self!
+              <p className='section-title'>Three: <span>Strike a pose & shoot :P</span></p>
               <video ref={handleVideoRef} />
               <canvas
                 ref={handleCanvasRef}
@@ -132,28 +334,29 @@ function App(props) {
                 height={2}
                 onClick={handleCapture}
               />
+              <p>SCROLL DOWN.</p>
             </section>
-            <section className={classes.Gallery}>
-              Step 4: Cherish this moment forever
               {picture && (
+              <section className={classes.Footer}>
                 <div className={classes.Picture}>
+                  <p>Four: <span>Cherish your shot!</span></p>
                   <img src={picture.dataUri} />
                   <h3>{picture.title}</h3>
                 </div>
+              </section>
               )}
-            </section>
           </main>
         </Route>
         /** * Readme route */
         <Route path="/readme">
-          <main>
-            <h2>Devtest Readme</h2>
+          <main className={classes.Readme}>
+            <h2>Devtest Readme.</h2>
+            <section>
             <p>
               Hello candidate, Welcome to our little dev test. The goal of this
               exercise, is to asses your general skill level, and give us
               something to talk about at our next appointment.
             </p>
-            <section>
               <h3>What this app should do</h3>
               <p>
                 SlapSticker is an app that lets users to slap stickers on their
@@ -161,6 +364,7 @@ function App(props) {
                 the ui needs some love. We'd like for you to extend this
                 prototype to make it look and feel it bit better.
               </p>
+              <br />
               <p>These are the basic requirements:</p>
               <ul>
                 <li>User can pick a sticker</li>
@@ -168,11 +372,9 @@ function App(props) {
                 <li>User can place the sticker over the webcam image</li>
                 <li>User can capture the webcam image with sticker</li>
               </ul>
-            </section>
-            <section>
               <h3>What we want you to do</h3>
               <p>
-                Off course we didn't expect you to build a full fledged app in
+                Of course we didn't expect you to build a full fledged app in
                 such a short time frame. That's why the basic requirements are
                 already implemented.
               </p>
@@ -181,6 +383,7 @@ function App(props) {
                 developer by improving the app.
               </p>
               <p>Some ideas (no need to do all):</p>
+              <br />
               <ul>
                 <li>Make it look really nice</li>
                 <li>Let users pick from multiple (custom) stickers</li>
@@ -191,17 +394,13 @@ function App(props) {
                 <li>Organize, document and test the code</li>
                 <li>Integrate with zoom, teams, meet...</li>
               </ul>
-            </section>
-            <section>
-              <h3> quickstart</h3>
+              <h3>Quickstart</h3>
               <ul>
                 <li>You can clone this repo to get started </li>
                 <li>run `$ npm install` to install deps</li>
                 <li>run `$ npm run start` to start dev environment</li>
                 <li>push it to github or gitlab to share it with us. </li>
               </ul>
-            </section>
-            <section>
               <p>
                 P.s. We've already added some libraries to make your life easier
                 (Create React App, Jss, React Router), but feel free to add
--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,18 @@
 export const theme = {
   palette: {
-    primary: "#2266dd",
+    grey_background: "#eee",
+    black_txt: "black",
+    red_primary: "#FE5F55",
+    red_secondary: "#885053",
+    red_nav:"#C35854",
+    gallery_background:"#FCCDB5",
     secondary: "#96CAA8",
-    background: "#eee",
     text: "#fff",
+    stickers_background: "#D4DFC7",
   },
+  filters: {
+    grey_shadow: "drop-shadow(0 0 5px grey)",
+    light_grey_shadow: "drop-shadow(0 0 2px #b3b3b3)",
+    canvas_shadow: "drop-shadow(0 0 5px #885053)",
+  }
 };
